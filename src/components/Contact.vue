b8e1ce5ff02f89497cd982260adf36a86ca4100c
rahal <rahalbechara@gmail.com>
2018-12-02 23:17:04 +0000

    done

---
+++ src/components/Contact.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="contact">
+    <h1>Contact us</h1>
+    <p>
+     Form
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact'
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import About from '@/components/About'
+import Contact from '@/components/Contact'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+    ,
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    }
+    ,
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    }
+  ]
+})
